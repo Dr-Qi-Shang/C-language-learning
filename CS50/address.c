1b47567f0be52f7b526810df6d959ba9a5206f30
Dr-Qi-Shang <ylks2753521414@163.com>
2026-04-04 20:02:24 +0800

    address

--- CS50/address.c
+++ CS50/address.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 int main(void)
 {
-    int n = 50;
-    int *p=&n;
-    printf("%i\n", *p);
+    char *s="EMMA";
+    printf("%c\n",*s);
+    printf("%c\n",*(s+1));
+    printf("%c\n",*(s+2));
+    printf("%c\n",*(s+3));
 }
